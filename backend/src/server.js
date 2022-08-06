2d4dbb29251c9a1cc8d84be49a040eef4dfd58af
Soualo <fernandsoualo@gmail.com>
2022-08-06 22:00:14 +0100

    make server serve static index.html file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 
 import { withDB } from './data';
 
@@ -7,6 +8,7 @@ const PORT = 8000;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '/build')));
 app.use(bodyParser.json());
 
 app.get('/api/articles/:name', async (req, resp) => {
@@ -60,6 +62,10 @@ app.post('/api/articles/:name/add-comment', async (req, resp) => {
 
 		resp.status(200).json(updatedArticleInfo);
 	}, resp);
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '/build/index.html'));
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
